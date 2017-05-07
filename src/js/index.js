0a5cbad6053a72881940526aad0e6343f7007c5f
Reinaldo Ferreira <reinaldocesarferreira@gmail.com>
2017-05-07 18:30:48 +0100

    displaying user options

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../main.min.css'
-import { on, getElement, toggleClass } from './helpers/helpers'
+import { on, getElement, toggleClass, getEventTarget, removeClass,
+  addClass } from './helpers/helpers'
 import { runAfterPromise } from './bookingTable/bookingTemplate'
 import { getAvailability } from './api/booking'
 
@@ -8,8 +9,16 @@ const btnLoad = document.getElementById('bookingSearch')
 const startingDate = document.getElementById('startingDate')
 const cleaningDuration = document.getElementById('cleaningDuration')
 const bookingTable = document.getElementById('bookingTable')
+const userDate = document.getElementById('bookingDate')
+const userTime = document.getElementById('bookingTime')
+const userDuration = document.getElementById('bookingDuration')
 
 var queryParams = '?weekBeginning=2016-12-05&visitDuration=2.5&postcode=EC1R%203BU'
+var bookingTo = {
+  day: '',
+  time: '',
+  duration: 0.5
+}
 
 // get promise done
 getAvailability(queryParams).then(data => runAfterPromise(data))
@@ -31,5 +40,29 @@ on(btnLoad, 'click', () => {
     setTimeout(function() {
       toggleClass(getElement('.error-message'), 'is-active')
     }, 3000)
+  }
+})
+
+on(bookingTable, 'click', () => {
+  event.stopPropagation()
+  let target = getEventTarget()
+  let arrOfItems = document.getElementsByClassName('is-available')
+  bookingTo = {
+    day: target.dataset.day,
+    time: target.dataset.starttime,
+    duration: cleaningDuration.value ? cleaningDuration.value : 0.5
+  }
+
+  // removing is-active from all available items
+  removeClass(arrOfItems, 'is-active')
+
+  // adding is-active to the selected available item
+  if (getElement(target).classList.contains('is-available')) {
+    toggleClass(getElement(target), 'is-active')
+    addClass('.user-booking', 'is-active')
+    // Displaying user selections
+    getElement(userDate).innerHTML = bookingTo.day
+    getElement(userTime).innerHTML = bookingTo.time
+    getElement(userDuration).innerHTML = bookingTo.duration
   }
 })
