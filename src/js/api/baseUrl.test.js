5df5dbe8135a0b0d20598194d7a8ba7ebb14f08b
Reinaldo Ferreira <rferreira@macmillan.org.uk>
2017-05-10 14:15:07 +0100

    baseUrl.test.js passing :tada:

--- src/js/api/baseUrl.test.js
+++ src/js/api/baseUrl.test.js
@@ -3,27 +3,13 @@ import jsdom from 'jsdom'
 import { getBaseUrl, BOOKING_API } from './baseUrl'
 
 describe('getBaseUrl()', () => {
-  it('should return the dev url', (done) => {
-    jsdom.env('', {
-      url: 'http://localhost:3000/?inDev',
-      contentType: 'text/html',
-      includeNodeLocations: true
-    }, (err, window) => {
-      expect(getBaseUrl(window)).to.equal('http://localhost:3030')
-      done()
-      window.close()
-    })
+  it('should return the dev url', () => {
+    let w = { location: { search: 'dev' } }
+    expect(getBaseUrl(w)).to.equal('http://localhost:3030')
   })
 
-  it('should return the production url', (done) => {
-    jsdom.env('', {
-      url: 'http://asdbooking.surge.sh/',
-      contentType: 'text/html',
-      includeNodeLocations: true
-    }, (err, window) => {
-      expect(getBaseUrl(window)).to.equal(BOOKING_API)
-      window.close()
-      done()
-    })
+  it('should return the production url', () => {
+    let w = { location: { search: '' } }
+    expect(getBaseUrl(w)).to.equal(BOOKING_API)
   })
 })
