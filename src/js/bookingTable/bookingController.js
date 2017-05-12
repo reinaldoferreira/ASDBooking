99e96b423ae3b8d421001a84a2b24e334c04994e
Reinaldo Ferreira <rferreira@macmillan.org.uk>
2017-05-12 15:29:53 +0100

    cleaning up

--- src/js/bookingTable/bookingController.js
+++ src/js/bookingTable/bookingController.js
@@ -1,16 +1,13 @@
 import { daysMatrix, timeList } from './bookingUI'
 
 // body to be executed once promise is done
-export const runAfterPromise = (data) => {
+export function runAfterPromise(data) {
   // Testing if data is right
   if (!Array.isArray(data) || data.length === 0) {
-    global.document.getElementById('bookingTable').innerHTML = '<p>Nothing found</p>'
+    document.getElementById('bookingTable').innerHTML = '<p>Nothing found</p>'
     return false
+  } else {
+    // Print template on html
+    document.getElementById('bookingTable').innerHTML = timeList(data) + daysMatrix(data)
   }
-
-  // join timeList and daysAndPossibilities to be printed
-  let template = timeList(data) + daysMatrix(data)
-
-  // Print template on html
-  global.document.getElementById('bookingTable').innerHTML = template
 }
